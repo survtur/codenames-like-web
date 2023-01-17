4ab365b38e6ddb513eadf9bc7152d8b50e0cf262
survtur <survtur@yandex.ru>
2023-01-17 18:01:10 +0300

    Simple localization. Black card rules update. Basic EN cards.

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import random
 
 from cards.cards import get_cards
 from schemas.game_state import Card, FullGameState
+from schemas.rules import Rules
 
 
 class SpecialTeam(enum.IntEnum):
@@ -23,36 +24,41 @@ def update_game_stats(s: FullGameState):
 
 
 def _update_winner(s: FullGameState):
-    """Set winner if there was no winner"""
+    """
+    Set winner if there was no winner.
+    Black team wins if black cards exist and all of them was opened.
+    Other team can win if its score is 0.
+    """
     if s.winner is None:
-        for c in s.cards:
-            if c.team == -2 and c.is_opened:
-                s.winner = -2
-                return
+        black_is_opened = [c.is_opened for c in s.cards if c.team == SpecialTeam.BLACK]
+        if black_is_opened and not any(black_is_opened):
+            s.winner = SpecialTeam.BLACK
+            return
 
         if 0 in s.score:
             s.winner = s.score.index(0)
 
 
-def create_game_set(rules) -> FullGameState:
-    assert sum(rules.team_cards_count) < rules.cards_count, 'Not enoungh cards to get enougn team cards'
-    assert min(rules.team_cards_count) > 0, 'Team cards should be greater than 0'
-    all_cards = rules.custom_cards or get_cards(rules.cards_set)
-    assert rules.cards_count <= len(all_cards), 'Not enoungh card in deck'
+def create_game_set(r: Rules) -> FullGameState:
+    assert sum(r.team_cards_count) + r.black_cards_count < r.cards_count, 'Not enoungh cards to get enougn team cards'
+    assert min(r.team_cards_count) > 0, 'Team cards should be greater than 0'
+    all_cards = r.custom_cards or get_cards(r.cards_set)
+    assert r.cards_count <= len(all_cards), 'Not enoungh card in deck'
     random.shuffle(all_cards)
 
-    cards = [Card(word=c, team=SpecialTeam.NEUTRAL, is_opened=False) for c in all_cards[:rules.cards_count]]
+    cards = [Card(word=c, team=SpecialTeam.NEUTRAL, is_opened=False) for c in all_cards[:r.cards_count]]
 
-    # Assign BLACK CARD
-    cards[0].team = SpecialTeam.BLACK
+    # Assign BLACK CARDS
+    for i in range(r.black_cards_count):
+        cards[i].team = SpecialTeam.BLACK
 
     # Assign other teams
-    index = 1  # Start with index 1, because index 0 is BLACK
-    for team_id, card_count in enumerate(rules.team_cards_count):
+    index = r.black_cards_count  # Start with index 1, because index 0 is BLACK
+    for team_id, card_count in enumerate(r.team_cards_count):
         for n in range(card_count):
             cards[index].team = team_id
             index += 1
 
     random.shuffle(cards)
-    return FullGameState(name="", cards=cards, winner=None, score=rules.team_cards_count)
+    return FullGameState(name="", cards=cards, winner=None, score=r.team_cards_count)
 
--- schemas/rules.py
+++ schemas/rules.py
@@ -5,4 +5,5 @@ class Rules(BaseModel):
     cards_count: int
     cards_set: int
     team_cards_count: list[int]
+    black_cards_count: int
     custom_cards: list[str] | None
